64d8d5eb27bfe4700062401104d84f04fc50c4ca
Suthar345Piyush <piyushsuthar524@gmail.com>
2026-06-25 09:02:54 +0530

    timeline tests done, working on core clipping part

--- internal/timeline/service_test.go
+++ internal/timeline/service_test.go
@@ -111,7 +111,7 @@ func isEqual(a, b float64) bool {
 	return math.Abs(a-b) < 0.001
 }
 
-// testing the video segment on 90 sec video, it will have three parts 0-30, 30-60, 60-90
+// testing the video segment on 90 sec video, it will have three exact fixed parts 0-30, 30-60, 60-90
 
 func TestBuildSegments_FixedParts(t *testing.T) {
 
@@ -138,7 +138,104 @@ func TestBuildSegments_FixedParts(t *testing.T) {
 		if !isEqual(w.Duration, 30) {
 			t.Errorf("segment duration should be 30: %v", w.Duration)
 		}
+	}
+}
 
+// function for last-unused clip time, removal
+
+// the full clips should be only 30 seconds and with minimum length of 15 seconds
+// video length is 70 - cut down to 0-30, 30-60, 60-70, last ten minutes will be removed
+func TestBuildSegments_LastCutPart(t *testing.T) {
+
+	ops := Options{ClipLength: 30, MinLength: 15, Overlap: 0}
+	segments := buildSegments(70, ops)
+
+	if len(segments) != 2 {
+		t.Fatalf("got %d segment, want only 2 segments (last 10 seconds removed)", len(segments))
+	}
+
+	if !isEqual(segments[1].EndTime, 60) {
+		t.Errorf("last segment end to 60, but got %.3f", segments[1].EndTime)
+	}
+
+}
+
+// test function for overlap
+// duration = 60s with overlap of 5s
+func TestBuildSegments_WithOverlap(t *testing.T) {
+
+	// a 60s clip with (0-30), (25-55), (50-60)
+
+	ops := Options{ClipLength: 30, MinLength: 15, Overlap: 5}
+	segments := buildSegments(60, ops)
+
+	if len(segments) != 2 {
+		t.Fatalf("got this %d segments, but want 2 only", len(segments))
+	}
+
+	// for 0 - 30
+
+	if !isEqual(segments[0].StartTime, 0) || !isEqual(segments[0].EndTime, 30) {
+		t.Errorf("segment 0 has : [%.3f, %.3f], but want something like this [0, 30]", segments[0].StartTime, segments[0].EndTime)
+	}
+
+	// for 25-55
+
+	if !isEqual(segments[1].StartTime, 25) || !isEqual(segments[1].EndTime, 55) {
+		t.Errorf("segment 1 has : [%.3f, %.3f], but want something like this [25, 55]", segments[1].StartTime, segments[1].EndTime)
+	}
+
+}
+
+// if the duration of clip, less than clip length
+// video duration - 10s, pair will [0, 10] one segment
+
+func TestBuildSegments_ShorterThanClipLength(t *testing.T) {
+
+	ops := Options{ClipLength: 30, MinLength: 5, Overlap: 0}
+	segments := buildSegments(10, ops)
+
+	if len(segments) != 1 {
+		t.Fatalf("got this %d segments, but want 1 only", len(segments))
+	}
+
+	if !isEqual(segments[0].StartTime, 0) || !isEqual(segments[0].EndTime, 10) {
+		t.Errorf("segment 0 has [%.3f, %.3f], but want [0, 10]", segments[0].StartTime, segments[0].EndTime)
+	}
+
+}
+
+// test function, is the video duration itself less than minimum length  (5s)
+
+func TestBuildSegments_VideoDurationShorterThanMinLength(t *testing.T) {
+
+	ops := Options{ClipLength: 20, MinLength: 5, Overlap: 0}
+	segments := buildSegments(3, ops)
+
+	if len(segments) != 0 {
+		t.Fatalf("video duration %d is less then minimum clip length", len(segments))
+	}
+
+}
+
+// options validate tests
+
+func TestOptionsValidate_DefaultVal(t *testing.T) {
+
+	ops := Options{ClipLength: 30}
+
+	if err := ops.validate(); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	// min length tests
+
+	if !isEqual(ops.MinLength, 15) {
+		t.Errorf("min length default: %.3f, want around 15", ops.MinLength)
+	}
+
+	if ops.Strategy != StrategyFixed {
+		t.Error("by default strategy should be StrategyFixed", StrategyFixed)
 	}
 
 }
